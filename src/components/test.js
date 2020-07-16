e7a4c3a1ab4476655b948802e29e8602d64980d0
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-16 17:54:57 +1000

    added a fetch request to test CORS on deployed site

--- src/components/test.js
+++ src/components/test.js
@@ -1,10 +1,19 @@
 import React, { Component} from 'react';
 
 export default class exerciseList extends Component {
+
+
+    componentDidMount = () => {
+        fetch('http://roastme-recipes.herokuapp.com/test')
+        .then(response => response.json())
+        .then(data => this.setState({data: data}));
+    }
+
     render() {
         return (
             <div>
-                <p>test</p>
+                <p>{this.state?.data.name}</p>
+                <p>{this.state?.data.age}</p>
             </div>
         )
     }
