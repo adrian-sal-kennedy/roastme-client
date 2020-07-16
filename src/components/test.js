1c7f29992e6dc3d2b4b32f06378667e81e09f3cd
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-16 18:06:19 +1000

    made the fetch call use https instead of http

--- src/components/test.js
+++ src/components/test.js
@@ -4,7 +4,7 @@ export default class exerciseList extends Component {
 
 
     componentDidMount = () => {
-        fetch('http://roastme-recipes.herokuapp.com/test')
+        fetch('https://roastme-recipes.herokuapp.com/test')
         .then(response => response.json())
         .then(data => this.setState({data: data}));
     }
