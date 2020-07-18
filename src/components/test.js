7c48e7cc6eff1c02d9054bce45f76a785c7f1caa
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-07-18 19:07:03 +1000

    start on navburger

--- src/components/test.js
+++ src/components/test.js
@@ -1,20 +1,25 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
 
 export default class test extends Component {
+  constructor(props) {
+    super(props);
+    const loadmsg = "loading...";
+    this.state = { data: { name: loadmsg, age: loadmsg } };
+  }
 
+  componentDidMount = () => {
+    // fetch('http://220.240.139.117:3000/test')
+    fetch("https://roastme-recipes.herokuapp.com/test")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data: data }));
+  };
 
-    componentDidMount = () => {
-        fetch('https://roastme-recipes.herokuapp.com/test')
-        .then(response => response.json())
-        .then(data => this.setState({data: data}));
-    }
-
-    render() {
-        return (
-            <div>
-                <p>name: {this.state?.data.name}</p>
-                <p>age: {this.state?.data.age}</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>name: {this.state?.data.name}</p>
+        <p>age: {this.state?.data.age}</p>
+      </div>
+    );
+  }
 }
