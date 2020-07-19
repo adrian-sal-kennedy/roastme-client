40fba963813c474b6df88e8e51b27465579b33d2
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-07-20 00:21:23 +1000

    implementing bulma components without hooks

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,68 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
+import {
+  Container,
+  Button,
+  Field,
+  Control,
+  Label,
+  Input,
+} from "react-bulma-components/dist";
 
 export default class login extends Component {
+  render() {
+  const [form, setForm] = useState({ name: "", password: "" });
+  const update = ({ target }) =>
+    setForm({ ...form, [target.name]: target.value });
+    return (
+      <>
+        <div>
+          <p>login component</p>
+        </div>
 
-    render() {
-        return (
-            <div>
-                <p>login component</p>
-            </div>
-        )
-    }
+        <Field>
+          <Control>
+            <Label>Name</Label>
+            <Input name="name" value={form.name} onChange={update} />
+          </Control>
+        </Field>
+      </>
+    );
+  }
 }
+
+// const Element = () => {
+//   const [form, setForm] = useState({ name: "", password: "" });
+//   const update = ({ target }) =>
+//     setForm({ ...form, [target.name]: target.value });
+//   return (
+//     <>
+//       <Field>
+//         <Control>
+//           <Label>Name</Label>
+//           <Input name="name" value={form.name} onChange={update} />
+//         </Control>
+//       </Field>
+//       <Field>
+//         <Control>
+//           <Label>Password</Label>
+//           <Input
+//             name="password"
+//             type="password"
+//             value={form.password}
+//             onChange={update}
+//           />
+//         </Control>
+//       </Field>
+//       <Button.Group>
+//         <Button
+//           fullwidth
+//           rounded
+//           color="primary"
+//           onClick={() => console.log(form)}
+//         >
+//           Login
+//         </Button>
+//       </Button.Group>
+//     </>
+//   );
+// };
