6ea255dc22aac920810686b3b163eeefaf2c93a3
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-23 12:13:09 +1000

    test

--- src/components/test.js
+++ src/components/test.js
@@ -3,22 +3,28 @@ import React, { Component } from "react";
 export default class test extends Component {
   constructor(props) {
     super(props);
-    const loadmsg = "loading...";
-    this.state = { data: { name: loadmsg, age: loadmsg } };
   }
 
   componentDidMount = () => {
     // fetch('http://220.240.139.117:3000/test')
-    fetch("https://roastme-recipes.herokuapp.com/test")
+      fetch("https://roast-me-recipes.herokuapp.com/")
       .then((response) => response.json())
       .then((data) => this.setState({ data: data }));
   };
 
   render() {
+    console.log(this.state?.data)
+    const recipes = this.state?.data.map(recipe => {
+      return(
+        <>
+        <p>{recipe.recipe.title}</p>
+        <p>{recipe.author.username}</p>
+        </>
+      )
+    })
     return (
       <div>
-        <p>name: {this.state?.data.name}</p>
-        <p>age: {this.state?.data.age}</p>
+        <p>{recipes || "loading"}</p>
       </div>
     );
   }
