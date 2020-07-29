062fda91f5df30759185311fe52dab8669131891
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-29 18:35:45 +1000

    added random images to recipe cards

--- src/shared/RecipeCard.js
+++ src/shared/RecipeCard.js
@@ -2,23 +2,25 @@ import React from "react";
 import moment from "moment";
 import Markdown from "react-markdown";
 import { Link } from "react-router-dom";
-import { Heading, Box, Container } from "react-bulma-components";
+import { Heading, Box, Container, Media, Image } from "react-bulma-components";
 import Taglist from "./TagList";
 
 export default function RecipeCard(props) {
   const { recipe, author, tags } = props.recipe;
   const createdAt = moment(recipe.created_at).startOf("hour").fromNow();
+
   return (
     <Box style={{ width: "100%", height: "100%" }}>
       <Container style={{ display: "flex", flexDirection: "column" }}>
         <Link className="fill-parent-link" to={`recipe/${recipe.id}`} />
         <Heading size={4}>{recipe.title}</Heading>
-        <Markdown
-          includeNodeIndex={true}
-          className="md font-1rem truncate"
-          source={recipe.blog}
-          // style={{ overflow: "scroll" }}
-        />
+        <Media.Item renderAs="figure" position="left">
+              <Image
+                size="16by9"
+                alt="64x64"
+                src={`pics/${Math.floor(Math.random() * 26)}.jpg`}
+              />
+            </Media.Item>
         <Heading subtitle>{author.username}</Heading>
         <time>{`${createdAt}`}</time>
       </Container>
