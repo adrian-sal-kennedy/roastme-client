98dfc4269753db0f47a4a57ee47149ecf1884954
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 15:52:29 +1000

    added retrieval of posts and passing them to component

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,12 +2,43 @@ import React, { Component} from 'react';
 import Post from '../shared/Post';
 
 export default class profile extends Component {
+    async componentDidMount() {
+        try {
+        const response = await fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/user`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                }
+            }
+        );
+        if (response.status >= 400) {
+            throw new Error("You must be logged in to do this!");
+        } else {
+            const posts= await response.json();
+            
+            this.setState({ posts: posts });
+        }
+        } catch (err) {
+        this.setState({
+            errMessage: err.message,
+        });
+        }
+    }
 
     render() {
         return (
             <div>
                 <p>profile component</p>
-                < Post />
+                {this.state?.posts &&
+                    this.state?.posts.map((post, idx) => {
+                    return (
+                        <div className="main-component flex-tile" key={idx + 1}>
+                        {<Post username={post.author.username} title={post.recipe.title} blog={post.recipe.blog} post={post.post.post} recipeId={post.recipe.id} userId={post.author.id} />}
+                        </div>
+              );
+            })}
             </div>
         )
     }
