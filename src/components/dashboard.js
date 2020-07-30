f455f62c42a1dc7cbf356f644aba5495d1a25e66
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 15:30:02 +1000

    added cards to display user posts, with links and fetch requests all included

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,11 +3,43 @@ import Post from '../shared/Post';
 
 export default class dashboard extends Component {
 
+    async componentDidMount() {
+        try {
+        const response = await fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/dashboard`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                }
+            }
+        );
+        if (response.status >= 400) {
+            throw new Error("You must be logged in to do this!");
+        } else {
+            const posts= await response.json();
+            
+            this.setState({ posts: posts });
+        }
+        } catch (err) {
+        this.setState({
+            errMessage: err.message,
+        });
+        }
+    }
+
     render() {
         return (
             <div>
                 <p>dashboard component</p>
-                < Post />
+                {this.state?.posts &&
+                    this.state.posts.map((post, idx) => {
+                    return (
+                        <div className="main-component flex-tile" key={idx + 1}>
+                        {<Post username={post.author.username} title={post.recipe.title} blog={post.recipe.blog} post={post.post.post} recipeId={post.recipe.id} userId={post.author.id} />}
+                        </div>
+              );
+            })}
             </div>
         )
     }
